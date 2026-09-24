4271c8cd803761deada7247cbc02c8f0f954f121
measure-of-electricity <w.wattsjr09@gmail.com>
2026-09-24 14:14:22 -0400

    Add files via upload

---
+++ projects/javascript/project04/project01-04_txt.js
@@ -0,0 +1,12 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: William Watts
+    Date: 9/24/2026
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
